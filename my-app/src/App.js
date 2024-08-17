8ee3370343a61871618fb055f696eb8d82b0107e
Edward1217 <m932012@gmail.com>
2024-08-17 16:07:41 -0700

    image slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
-import StarRating from './components/star-rating'
+import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 function App() {
 
   return (
     <div className="App">
       {/* Accordian component */}
-      <Accordian/>
+      {/* <Accordian/> */}
 
       {/* Random color component */}
-      <RandomColor/>
+      {/* <RandomColor/> */}
       {/* star rating component */}
-      <StarRating noOfStarts={10}/>
+      {/* <StarRating noOfStarts={10}/> */}
+      {/* image slider component */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page ={"1"}limit={"10"}/>
 
     </div>
     
