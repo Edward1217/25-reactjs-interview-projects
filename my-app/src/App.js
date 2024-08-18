f52690c413adc9fa6e5693612c57acb61eda3b63
Edward1217 <m932012@gmail.com>
2024-08-17 22:53:11 -0700

    load more data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,22 @@ import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 function App() {
 
   return (
     <div className="App">
       {/* Accordian component */}
-      {/* <Accordian/> */}
+      <Accordian/>
 
       {/* Random color component */}
       {/* <RandomColor/> */}
       {/* star rating component */}
       {/* <StarRating noOfStarts={10}/> */}
       {/* image slider component */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} page ={"1"}limit={"10"}/>
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page ={"1"}limit={"10"}/> */}
+      {/* load more data */}
+      {/* <LoadMoreData/> */}
 
     </div>
     
