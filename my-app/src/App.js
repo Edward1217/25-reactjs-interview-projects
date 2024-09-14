aad75360c57b130a0869de1fcfa80c80ba458b66
Edward1217 <m932012@gmail.com>
2024-09-13 19:39:24 -0700

    scroll indicaator

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-data';
-import TreeView from './components/tree-view'
-import menus from './components/tree-view/data';
-import Button from './components/qr-code-generator';
-import LightDarkMode from './components/light-dark-mode'
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
+import Button from "./components/qr-code-generator";
+import LightDarkMode from "./components/light-dark-mode";
+import ScrollIndicator from "./components/scroll-indicator";
 function App() {
-
   return (
     <div className="App">
       {/* Accordian component */}
@@ -28,9 +28,10 @@ function App() {
       {/* Qr code generator */}
       {/* <Button/> */}
       {/* Light Dark Mode */}
-      <LightDarkMode/>
+      {/* <LightDarkMode/> */}
+      {/* scoll indicator */}
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
     </div>
-    
   );
 }
 
